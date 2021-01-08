36a31e3c1d5880921cc813e3bd259ee54f2b3940
forteller11 <charly.yan.miller@gmail.com>
2021-01-08 12:26:15 -0500

    finished rough move method, architecting drawing

---
+++ chess_proj/Discord/Renderer.cs
@@ -0,0 +1,12 @@
+﻿using chess_proj.Systems;
+
+namespace chess_proj.Discord
+{
+    public class Renderer
+    {
+        public void Render(Board board)
+        {
+            //todo embed emoji
+        }
+    }
+}
--- chess_proj/Systems/Grid.cs
+++ chess_proj/Systems/Grid.cs
@@ -1,12 +1,15 @@
-﻿using chess_proj.Math;
+﻿using System.Collections.Generic;
+using chess_proj.Math;
 using chess_proj.Systems.Pieces;
+using Discord;
 
 #nullable enable
 namespace chess_proj.Systems
 {
     public class Board
     {
-        public readonly Piece[]?[]? Cells;
+        public readonly Piece?[][] Cells;
+        private readonly List<Int2> ValidMoves;
         public Player White = new Player();
         public Player Black = new Player();
         
@@ -21,14 +24,45 @@ namespace chess_proj.Systems
             
         }
 
-        public void MovePiece(Int2 target)
+        public void MovePiece(Player actor, Int2 from, Int2 target)
         {
+            var piece = GetCell(from);
+
+            if (piece == null)
+            {
+                Program.DebugLog($"no piece at {from}");
+                return;
+            }
+            
+            //make sure piece is owned by actor
+            if (piece.Owner != actor)
+            {
+                Program.DebugLog($"{piece.Name} not owned by actor");
+                return;
+            }
+
+            //is target in list of valid moves
+            ValidMoves.Clear();
+            piece.RefreshValidMoves(in ValidMoves);
+            if (!ValidMoves.Contains(target))
+            {
+                Program.DebugLog($"Cannot move {piece.Name} to {target}");
+                return;
+            }
+
+            //remove any existing pieces
             if (GetCell(target) != null)
             {
-                
+                var takenPiece = GetCell(target);
+                takenPiece!.Owner.Captured.Add(takenPiece);
+                Cells[target.X][target.Y] = null;
             }
+            
+            //move piece
+            Cells[from.X][from.Y] = null;
+            Cells[target.X][target.Y] = piece;
         }
 
-        public Piece? GetCell(Int2 position) => Cells?[position.X][position.Y];
+        public Piece? GetCell(Int2 position) => Cells[position.X][position.Y];
     }
 }
